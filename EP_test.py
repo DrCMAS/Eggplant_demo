48aabe41f52dd17da253bb9e586167173f77cf19
DrCMAS <87641108+DrCMAS@users.noreply.github.com>
2021-07-19 09:34:13 +0100

    Add files via upload

---
+++ EP_test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+x=np.random.randint(0,100,20)
+y=np.random.randint(0,100,20)
+
+m,c=np.polyfit(x,y,1)
+
+plt.plot(x,y,'x')
+plt.plot(x,(m*x+c))
